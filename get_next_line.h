0c4467ea954cd1d01848ee60d1313a258f5a950f
Luis Ignacio Pastor Cristobal <lpastor-@c2r10s5.42madrid.com>
2023-09-26 11:53:02 +0200

    Added functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,19 @@
 /*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 07:54:34 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/09/22 07:55:38 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/09/26 11:50:06 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+size_t	gnl_strlen(char *str);
+char	*gnl_cpy(char *data);
+int		gnl_find_ch(char *data, char ch);
+void	*gnl_free(void **memory);
 
 char	*get_next_line(int fd);
 
