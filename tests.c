68c27735234eb5116816274ab41d87670a3bfe4e
Luiz-Pastor <luigpacr5@gmail.com>
2023-09-26 23:17:10 +0200

    .

--- tests.c
+++ tests.c
@@ -3,16 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   tests.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 11:49:15 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/09/26 11:50:14 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/09/26 22:15:04 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+#define FILE "files/read_error.txt"
 
 int	main(void)
 {
+	int fd;
+	int check;
+	char *line;
+
+	fd = open(FILE, O_RDONLY);
+
+	check = 1;
+	while (check)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		if (line)
+			free(line);
+		else
+			check = 0;
+	}
+
 	return (0);
 }
