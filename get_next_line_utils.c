ed84398796ccb29179bddeb6da7862cf6de385fb
Luis Ignacio Pastor Cristobal <lpastor-@c2r10s5.42madrid.com>
2023-09-26 11:53:20 +0200

    Added functions

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,63 @@
 /*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 08:02:16 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/09/22 08:02:22 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/09/26 11:47:14 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_utils.h"
+#include "get_next_line.h"
+
+size_t	gnl_strlen(char *str)
+{
+	size_t	index;
+
+	index = 0;
+	if (!str)
+		return (0);
+	while (str[index])
+		index++;
+	return (index);
+}
+
+char	*gnl_cpy(char *data)
+{
+	size_t	index;
+	char	*cpy;
+
+	index = 0;
+	if (!data)
+		return (NULL);
+	cpy = (char *) malloc(gnl_strlen(data) + 1);
+	if (!cpy)
+		return (NULL);
+	while (data[index])
+	{
+		cpy[index] = data[index];
+		index++;
+	}
+	cpy[index] = '\0';
+	return (cpy);
+}
+
+int	gnl_find_ch(char *data, char ch)
+{
+	size_t	index;
+
+	index = 0;
+	if (!data)
+		return (0);
+	while (data[index] && data[index] != ch)
+		index++;
+	if (data[index] == ch)
+		return (1);
+	return (0);
+}
+
+void	*gnl_free(void **memory)
+{
+	if (!memory || !(*memory))
+		return (NULL);
+	free(*memory);
+	*memory = NULL;
+	return (NULL);
+}
