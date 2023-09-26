fd478f9b7f53155db86d05da839b1b02e4735d05
Luis Ignacio Pastor Cristobal <lpastor-@c2r10s5.42madrid.com>
2023-09-26 11:53:48 +0200

    File to test

--- tests.c
+++ tests.c
@@ -1,44 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 11:49:15 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/09/26 11:50:14 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include "get_next_line.h"
 
-#define DEFAULT_FILE "files/empty"
-
-void	my_wait(void)
+int	main(void)
 {
-	char str[255];
-	// FILE *file = fopen(stdin, "r");
-	fgets(str, 254, stdin);
-}
-
-int main(int argc, char *argv[])
-{
-	int	fd;
-	char *line;
-	char *filename;
-
-
-	if (argc < 2)
-		filename = DEFAULT_FILE;
-	else
-		filename = argv[1];
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 1)
-		return (printf("No file\n"), 1);
-	// line = get_next_line(fd);
-	// printf("\t> Linea 1:\n");
-	// printf("%s\n", line);
-
-	// line = get_next_line(fd);
-	// printf("\t> Linea 2:\n");
-	// printf("%s\n", line);
-	line = get_next_line(fd);
-	printf("> %s\n", line);
-	free(line);
-	line = get_next_line(fd);
-	printf("> %s\n", line);
-	free(line);
+	return (0);
 }
