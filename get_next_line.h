9490ee8f7c760d484ee62ac282204fe99a89b971
Luiz-Pastor <luigpacr5@gmail.com>
2023-10-07 23:19:15 +0200

    Added missing function

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 07:54:34 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/09/27 09:21:27 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/10/07 23:14:50 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 size_t	gnl_strlen(char *str);
 char	*gnl_cpy(char *data);
 int		gnl_find_ch(char *data, char ch);
+char	*add_line(char *data, char *add);
 void	*gnl_free(void **memory);
 
 char	*get_next_line(int fd);
